17168ce3b8168a471bbc40832fa2fd3b210f5d86
Lei Li <lei.li@slalom.com>
2026-02-09 21:47:04 -0600

    Fix DNS

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,12 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
-const basePath = isProd ? '/wsbio' : '';
-
 const nextConfig = {
   output: 'export',
+
   images: {
     unoptimized: true,
   },
+
   trailingSlash: true,
-  basePath: basePath,
-  assetPrefix: isProd ? '/wsbio/' : '',
-  env: {
-    NEXT_PUBLIC_BASE_PATH: basePath,
-  },
 };
 
 export default nextConfig;
